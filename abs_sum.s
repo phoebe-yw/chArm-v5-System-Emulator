ab54addc0455313c7f810f7014583d0aa55bd06e
Hieu Chu <hieutc@cs.utexas.edu>
2026-04-22 13:56:56 -0500

    commented modfied version

--- abs_sum.s
+++ abs_sum.s
@@ -13,36 +13,36 @@ abs_sum:
     movz    x10, #2
     tst     x1, x9              // check odd or even
     b.ne    .handle_odd         // do odd case first
-    add     x1, x1, #2          // because jumped to middle must buffer)
-    b       .loop_check
+    add     x1, x1, #2          // because jumped to middle, +2 buffer
+    b       .loop_check 
 
 .loop2:
-    ldur    x3, [x0, #0]       
-    ldur    x7, [x0, #8]       
-    add     x0, x0, #16        
-    asr     x4, x3, #63        
+    ldur    x3, [x0, #0]        // load arr[i]
+    ldur    x7, [x0, #8]        // load arr[i+1]
+    add     x0, x0, #16         // increment i += 2
+    asr     x4, x3, #63         // compute additive inverse if negative and keep is positive
     asr     x8, x7, #63         
     eor     x3, x3, x4
     eor     x7, x7, x8
     subs    x3, x3, x4          
     subs    x7, x7, x8          
-    adds    x2, x2, x3          
+    adds    x2, x2, x3          // add the 2 longs
     adds    x2, x2, x7          
 
 .loop_check:
-    subs    x1, x1, x10         // decrement pair counter
+    subs    x1, x1, x10         // decrement x by 2
     b.ne    .loop2              // 1 misprediction at exit
     b       .done
 
-.handle_odd:
-    ldur    x3, [x0, #0]        // process odd case
-    add     x0, x0, #8
-    asr     x4, x3, #63
-    eor     x3, x3, x4
+.handle_odd:                    // process odd case
+    ldur    x3, [x0, #0]        
+    add     x0, x0, #8          // i += 1
+    asr     x4, x3, #63         
+    eor     x3, x3, x4          // compute additive inverse if negative
     subs    x3, x3, x4
-    adds    x2, x2, x3
-    subs    x1, x1, x9
-    add     x1, x1, #2
+    adds    x2, x2, x3          // add long
+    subs    x1, x1, x9          // decrement x by 1    
+    add     x1, x1, #2          // +2 buffer
     b.ne    .loop_check
 
 .done:                          // LOOP IS DONE
